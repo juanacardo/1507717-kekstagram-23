7b1f40dba494b4b38c2d8b52f4bfb92b38a733bb
juanacardo <juanacardo@gmail.com>
2021-07-04 15:03:43 +0500

    Отрисуй меня полностью (часть 2) вторая попытка

--- js/show-fullphoto.js
+++ js/show-fullphoto.js
@@ -3,28 +3,39 @@ import {userPhotos} from './thumbnails.js';
 const thumbnails = document.querySelectorAll('.picture__img');
 const fullPhoto = document.querySelector('.big-picture');
 const fullPhotoCloseButton = fullPhoto.querySelector('#picture-cancel');
+const fullPhotoCommentsList = fullPhoto.querySelector('.social__comments');
 
-for (let i = 0; i < thumbnails.length; i++) {
-  thumbnails[i].addEventListener('click', () => {
+thumbnails.forEach((thumbnail, index) => {
+  thumbnail.addEventListener('click', () => {
     fullPhoto.classList.remove('hidden');
-    fullPhoto.querySelector('img').src = userPhotos[i].url;
-    fullPhoto.querySelector('.likes-count').textContent = userPhotos[i].likes;
-    fullPhoto.querySelector('.comments-count').textContent = userPhotos[i].comments.length;
-    fullPhoto.querySelector('.social__caption').textContent = userPhotos[i].description;
+    fullPhoto.querySelector('img').src = userPhotos[index].url;
+    fullPhoto.querySelector('.likes-count').textContent = userPhotos[index].likes;
+    fullPhoto.querySelector('.comments-count').textContent = userPhotos[index].comments.length;
+    fullPhoto.querySelector('.social__caption').textContent = userPhotos[index].description;
     fullPhoto.querySelector('.social__comment-count').classList.add('hidden');
     fullPhoto.querySelector('.comments-loader').classList.add('hidden');
     document.querySelector('body').classList.add('modal-open');
+    userPhotos[index].comments.forEach((comment) => {
+      const fullPhotoComment = fullPhotoCommentsList.querySelector('.social__comment').cloneNode(true);
+      fullPhotoComment.querySelector('img').src = comment.avatar;
+      fullPhotoComment.querySelector('img').alt = comment.name;
+      fullPhotoComment.querySelector('.social__text').textContent = comment.message;
+      fullPhotoCommentsList.appendChild(fullPhotoComment);
+    });
   });
-}
+});
 
-fullPhotoCloseButton.addEventListener('click', () => {
+const hideFullPhoto = () => {
   document.querySelector('body').classList.remove('modal-open');
   fullPhoto.classList.add('hidden');
+};
+
+fullPhotoCloseButton.addEventListener('click', () => {
+  hideFullPhoto();
 });
 
 document.addEventListener('keydown', (evt) => {
   if (evt.keyCode === 27) {
-    document.querySelector('body').classList.remove('modal-open');
-    fullPhoto.classList.add('hidden');
+    hideFullPhoto();
   }
 });
