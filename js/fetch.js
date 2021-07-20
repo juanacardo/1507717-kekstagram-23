6962b9a1724d8a61770ca2c4ea907c8031993e57
juanacardo <juanacardo@gmail.com>
2021-07-20 17:41:10 +0500

    Доработки

--- js/fetch.js
+++ js/fetch.js
@@ -2,10 +2,10 @@ import {showAlert} from './utils.js';
 import {imgUploadForm, onUploadFormEscKeydown} from './form.js';
 import {setFormSuccessPopup, setFormErrorPopup} from './form-messages.js';
 
-const serverData = 'https://23.javascript.pages.academy/kekstagram/data';
-const serverPostAdress = 'https://23.javascript.pages.academy/kekstagram';
+const SERVER_DATA = 'https://23.javascript.pages.academy/kekstagram/data';
+const SERVER_POST_ADRESS = 'https://23.javascript.pages.academy/kekstagram';
 
-const getUserPhotos = () => fetch(serverData)
+const getUserPhotos = () => fetch(SERVER_DATA)
   .then((response) => {
     if (response.ok) {
       return response.json();
@@ -26,7 +26,7 @@ const setUserFormSubmit = (onSuccess, onError) => {
     const formData = new FormData(evt.target);
 
     fetch(
-      serverPostAdress,
+      SERVER_POST_ADRESS,
       {
         method: 'POST',
         body: formData,
